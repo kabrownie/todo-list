36f71b0289675b25b71b799df941d4dbe2716d04
kabrownie <makonokevin@gmail.com>
2023-07-05 15:46:43 +0300

    g

--- app/src/main/java/com/kabrownie/todo/MainActivity.java
+++ app/src/main/java/com/kabrownie/todo/MainActivity.java
@@ -2,13 +2,28 @@ package com.kabrownie.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent(MainActivity.this, dashoard.class);
+                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(mainIntent);
+                finish();
+            }
+
+        }, 2000);
     }
 }
---
+++ app/src/main/java/com/kabrownie/todo/dashoard.java
@@ -0,0 +1,14 @@
+package com.kabrownie.todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dashoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashoard);
+    }
+}
